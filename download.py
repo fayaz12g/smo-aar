e144a7cfafd88f803264cc143f0c428ae61058eb
Fayaz12 Gaming <61674751+fayaz12g@users.noreply.github.com>
2023-08-07 23:56:12 -0700

    base

---
+++ download.py
@@ -0,0 +1,55 @@
+import sys
+import os
+
+def download_extract_copy(input_folder):
+    import requests
+    import zipfile
+    import shutil
+    import getpass
+
+    # URL of the ZIP file
+    zip_url = f"https://github.com/fayaz12g/totk-controllers/raw/main/{controller_id}.zip"
+
+    username = getpass.getuser()
+    directory_path = f"C:/Users/{username}/AppData/Roaming/totk-aar/controllers"
+    # Check if the directory exists
+    if not os.path.exists(directory_path):
+        # Create the directory if it doesn't exist
+        os.makedirs(directory_path)
+        print(f"Directory {directory_path} created successfully.")
+    else:
+        print(f"Directory {directory_path} already exists.")
+    totk_folder = f"C:/Users/{username}/AppData/Roaming/totk-aar/controllers"
+    zip_file_source = os.path.join(totk_folder, f"{controller_id}.zip")
+
+    if not os.path.isfile(zip_file_source):
+        # Download the ZIP file
+        print("Downloading zip file. This may take up to 10 seconds.")
+        response = requests.get(zip_url)
+        print("Zip file downloaded.")
+        with open(zip_file_source, "wb") as file:
+            print("Writing contents to temp folder.")
+            file.write(response.content)
+
+    # Extract the ZIP file
+    extract_folder = os.path.join(output_folder, mod_name, "temp")
+    print(f"Extracting zip to {extract_folder}. This can also take a few seconds.")
+    with zipfile.ZipFile(zip_file_source, "r") as zip_ref:
+        zip_ref.extractall(extract_folder)
+
+    # Copy the extracted file
+    print("Copying extracted files")
+    romfs_folder = os.path.join(output_folder, mod_name, "romfs")
+    extracted_folder = os.path.join(extract_folder)
+    src_file_path = os.path.join(extracted_folder, "UI", "LayoutArchive", "Common.Product.110.Nin_NX_NVN.blarc.zs")
+    dst_file_path = os.path.join(romfs_folder, "UI", "LayoutArchive", "Common.Product.110.Nin_NX_NVN.blarc.zs")
+    os.makedirs(os.path.dirname(dst_file_path), exist_ok=True)
+    shutil.copy2(src_file_path, dst_file_path)
+    src_folder_path = os.path.join(extracted_folder, "Font")
+    dst_folder_path = os.path.join(romfs_folder, "Font")
+    os.makedirs(os.path.dirname(dst_folder_path), exist_ok=True)
+    shutil.copytree(src_folder_path, dst_folder_path)
+
+    # Clean up
+    print("Cleaning up old files")
+    shutil.rmtree(extract_folder)
--- script.py
+++ script.py
@@ -5,21 +5,50 @@ import libyaz0
 import struct
 import math
 import ast
+from compress import pack 
+from compress import pack_folder_to_blarc
+import customtkinter
+import tkinter
+from tkinter import filedialog
+from tkinter import scrolledtext
+from tkinter.filedialog import askdirectory
+from customtkinter import *
+from threading import Thread
 
-input_folder = None
+class PrintRedirector:
+    def __init__(self, text_widget):
+        self.text_widget = text_widget
+        self.buffer = ""
+        self.text_widget.configure(state='disabled')  # Disable user input
+        self.text_widget.tag_configure("custom_tag", background='lightgray', foreground='black')
 
-def main():
-    global input_folder
+    def write(self, text):
+        self.buffer += text
+        self.text_widget.configure(state='normal')  # Enable writing
+        self.text_widget.insert("end", text, "custom_tag")  # Apply custom_tag to the inserted text
+        self.text_widget.see("end")
+        self.text_widget.configure(state='disabled')  # Disable user input again
+
+    def flush(self):
+        self.text_widget.configure(state='normal')  # Enable writing
+        try:
+            self.text_widget.insert("end", self.buffer, "custom_tag")  # Apply custom_tag to the buffered text
+        except Exception as e:
+            self.text_widget.insert("end", f"Error: {e}\n", "custom_tag")  # Display the exception message with custom_tag
+        finally:
+            self.text_widget.see("end")
+            self.text_widget.configure(state='disabled')  # Disable user input again
+            self.buffer = ""
+
+scaling_factor = 0.762
+
+def main(input_folder):
+    global scaling_factor
+    scaling_factor = (16/9) / (int(numerator.get()) / int(denominator.get()))
 
     # download the Layout folder
     # calculate the scaling factor based on entered aspect ratio
     # generate the pchtxt file
-    
-    if len(sys.argv) != 2:
-        print("Usage: python script_name.py input_folder")
-        sys.exit(1)
-
-    input_folder = sys.argv[1]
 
     if not os.path.exists(input_folder):
         print(f"Error: Folder '{input_folder}' does not exist.")
@@ -42,7 +71,7 @@ def float2hex(f):
     return hex(struct.unpack('>I', struct.pack('<f', f))[0]).lstrip('0x').rjust(8,'0').upper()
 
 def patch_blyt(filename, pane, operation, value):
-    print("SHIFTING")
+    print(f"Scaling {pane} by {value}")
     offset_dict = {'shift_x': 0x40, 'shift_y': 0x48, 'scale_x': 0x70, 'scale_y': 0x78} 
     full_path = filename
     with open(full_path, 'rb') as f:
@@ -110,15 +139,64 @@ def extract_blarc(file, output_folder):
                 out.write(fileData)
 
             if extracted_file.endswith("bflyt"):
-                patch_blyt(extracted_file_path, "RootPane", "scale_x", 0.762)
+                patch_blyt(extracted_file_path, "RootPane", "scale_x", scaling_factor)
 
         layout_lyarc = os.path.join(root, "layout.lyarc")
         if os.path.exists(layout_lyarc):
             extract_blarc(layout_lyarc, root)
-            os.remove(layout_lyarc)
+            # os.remove(layout_lyarc)
 
-    os.remove(os.path.join(input_folder, file))
+    os.remove(file)
+
+def handle_focus_in(entry, default_text):
+    if entry.get() == default_text:
+        entry.delete(0, "end")
+        entry.configure(text_color=("#000000", "#FFFFFF"))
+
+def handle_focus_out(entry, default_text):
+    if entry.get() == "":
+        entry.insert(0, default_text)
+        entry.configure(text_color='gray')
+
+def select_mario_folder():
+    input_folder = askdirectory()
+    main(input_folder)
+
+def do_stuff():
+    sys.stdout = PrintRedirector(scrolled_text)
+    t = Thread(target=select_mario_folder)
+    t.start()
+
+root = customtkinter.CTk()
+root.title(f"AAR for Super Mario Odyssey")
+root.geometry("500x520")
+
+customtkinter.set_appearance_mode("system")
+customtkinter.set_default_color_theme("blue")  
+
+frame2 = customtkinter.CTkFrame(master=root)
+
+scrolled_text = scrolledtext.ScrolledText(master=root, width=50, height=18, font=("Helvetica", 10))
+scrolled_text.pack(pady=30)
+
+bntx_folder_button = customtkinter.CTkButton(master=root, text="Select Mario Layout Folder", fg_color="red", hover_color="pink", command=do_stuff)
+bntx_folder_button.pack(pady=15)
+
+numerator_var = StringVar(value="21")
+denominator_var = StringVar(value="9")
+
+ratiolabel = customtkinter.CTkLabel(root, text="Enter Aspect Ratio:")
+ratiolabel.pack()
+numerator = customtkinter.CTkEntry(root, textvariable=numerator_var)
+numerator.bind("<FocusIn>", lambda event: handle_focus_in(numerator, "21"))
+numerator.bind("<FocusOut>", lambda event: handle_focus_out(numerator, "21"))
+numerator.pack(side="left", padx=20)
+numerator.configure(text_color='gray')
+denominator = customtkinter.CTkEntry(root, textvariable=denominator_var)
+denominator.bind("<FocusIn>", lambda event: handle_focus_in(denominator, "9"))
+denominator.bind("<FocusOut>", lambda event: handle_focus_out(denominator, "9"))
+denominator.configure(text_color='gray')
+denominator.pack(side="right", padx=20)
 
 
-if __name__ == "__main__":
-    main()
+root.mainloop()
