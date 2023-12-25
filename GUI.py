8a9d7aad6490d799efd7b7de160b8d698232a3a8
Fayaz12 Gaming <61674751+fayaz12g@users.noreply.github.com>
2023-12-25 00:22:37 -0500

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -645,7 +645,7 @@ button_color_dropdown = customtkinter.CTkOptionMenu(master=notebook.tab("Control
 button_layout_label= customtkinter.CTkLabel(master=notebook.tab("Controller"), text="Button Layout:")
 button_layout_dropdown = customtkinter.CTkOptionMenu(master=notebook.tab("Controller"), variable=button_layout, values=full_button_layouts, command=update_image)
 
-notebook.delete("Controller")
+notebook.delete("Controller") # delete this line to readd controller options
 
 ###################
 ####### HUD #######
