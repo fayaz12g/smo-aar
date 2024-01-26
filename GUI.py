ac20bc8de3af75e3ea137aae5ad3bdd50799861c
Fayaz Shaikh <61674751+fayaz12g@users.noreply.github.com>
2024-01-26 00:51:22 -0500

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -35,7 +35,7 @@ from controller import controller_files
 #### Create Window ####
 #######################
 
-tool_version = "1.4.1"
+tool_version = "1.4.2"
 
 root = customtkinter.CTk()
 root.title(f"Fayaz's Settings {tool_version} for Super Mario Odyssey")
