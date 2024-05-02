e2faffe49fb07904624b5b39885a7bc2b2d64dbd
Fayaz Shaikh <61674751+fayaz12g@users.noreply.github.com>
2024-05-02 02:38:11 -0400

    more precise values

--- patch.py
+++ patch.py
@@ -1,17 +1,8 @@
 import os
 import math
+from functions import *
 
 def create_patch_files(patch_folder, ratio_value, scaling_factor, visual_fixes):
-
-    def make_hex(x, r):
-        p = math.floor(math.log(x, 2))
-        a = round(16*(p-2) + x / 2**(p-4))
-        if a<0: a += 128
-        a = 2*a + 1
-        h = hex(a).lstrip('0x').rjust(2,'0').upper()
-        hex_value = f'0{r}' + h[1] + '02' + h[0] + '1E' 
-        print(hex_value)
-        return hex_value
 
     visual_fixesa = visual_fixes[0]
     visual_fixesb = visual_fixes[1]
@@ -21,6 +12,7 @@ def create_patch_files(patch_folder, ratio_value, scaling_factor, visual_fixes):
     print(f"The scaling factor is {scaling_factor}.")
     hex_value = make_hex(ratio_value, 0)
     hex_value2 = make_hex(ratio_value, 3)
+    special_hex1, special_hex2 = make_specials(ratio_value)
     version_variables = ["1.0.0", "1.2.0", "1.3.0"]
     for version_variable in version_variables:
         file_name = f"main-{version_variable}.pchtxt"
@@ -38,11 +30,15 @@ def create_patch_files(patch_folder, ratio_value, scaling_factor, visual_fixes):
             replacement2_value = "00AA7494"
             visual_fix = visual_fixesc
 
-        elif version_variable == "1.3.0":
+        if version_variable == "1.3.0":
             nsobidid = "B424BE150A8E7D78701CBE7A439D9EBF"
-            replacement_value = "0074D2EC"
-            replacement2_value = "006329F8"
             visual_fix = visual_fixesb
+            patch_stuff = f'''0096816c {special_hex1}
+00968170 {special_hex2}
+0073de88 1F2003D5'''
+        else:
+            patch_stuff = f'''{replacement_value} {hex_value}
+{replacement2_value} {hex_value2}'''
 
         patch_content = f'''@nsobid-{nsobidid}
 
@@ -50,9 +46,8 @@ def create_patch_files(patch_folder, ratio_value, scaling_factor, visual_fixes):
 @flag offset_shift 0x100
 
 @enabled
-{replacement_value} {hex_value}
-{replacement2_value} {hex_value2}
-@stop
+{patch_stuff}
+@disabled
 
 {visual_fix}
 
--- visuals.py
+++ visuals.py
@@ -21,12 +21,12 @@ def create_visuals(do_screenshot, do_disable_fxaa, do_disable_dynamicres):
 00A56568 68008052
 00A55CD0 1F2003D5
 00A55CD4 1F2003D5
-@stop
+@disabled
 
 // Disable FXAA
 @{disablefxaa}
 00B92318 08000014
-@stop
+@disabled
 
 // Disable Dynamic Resolution
 @{disabledynamicres}
@@ -37,19 +37,28 @@ def create_visuals(do_screenshot, do_disable_fxaa, do_disable_dynamicres):
 
     visuals1_3_0= f'''// Screenshot Mode Graphics
 @{screenshot}
-005EE238 68008052
-005EDB3C 1F2003D5
-005EDB40 1F2003D5
-@stop
+008fe52c 00103E1E
+0098b648 E003271E
+@disabled
 
 // Disable FXAA
 @{disablefxaa}
 009BD3A8 08000014
-@stop
+@disabled
 
 // Disable Dynamic Resolution
 @{disabledynamicres}
 005EE438 1F2003D5
+004e27bc 03408152
+004e27c0 04B48052
+004e27d8 03408152
+004e27dc 04B48052
+004bf34c 04408152
+004bf350 05B48052
+004bf354 06408152
+004bf358 07B48052
+004bf2a4 094081D2
+004bf2a8 09B4C0F2
 @stop
 '''
 
@@ -58,12 +67,12 @@ def create_visuals(do_screenshot, do_disable_fxaa, do_disable_dynamicres):
 00A56568 68008052
 00A55CD0 1F2003D5
 00A55CD4 1F2003D5
-@stop
+@disabled
 
 // Disable FXAA
 @disabled
 00B92318 08000014
-@stop
+@disabled
 
 // Disable Dynamic Resolution
 @disabled
